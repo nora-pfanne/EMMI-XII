fb168435aa815b9008abc5b288a3b7f3e579f43b
norablakaj <nora.blakaj@leg-uelzen.de>
2019-02-25 16:00:00 +0100

    ggT2

--- ggT.c
+++ ggT.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
-unsigned int g, m, n;
 
-int ggt(unsigned int m, unsigned int n) {
+// klassischer Euklid-Algorithmus
+int ggT1(unsigned int m, unsigned int n) {
 	
+	// Überprüfung, ob es sich um die selbe Zahl handelt
 	while(m != n) {
+		
+		if(n == 0 || m == 0) {
+			
+			return 0;
 	
-		if(m > n) {
+		} else if(m > n) {
 			
 			m = m - n;
 		} else {
@@ -18,12 +23,37 @@ int ggt(unsigned int m, unsigned int n) {
 	return m;
 }	
 
+int ggT2(unsigned int m, unsigned int n) {
+	
+	unsigned int temp;
+	
+	if(m == 0 || n == 0){
+		
+		return 0;
+	}	
+	
+	while(n != 0) {
+		
+		temp = m;
+		m = n;
+		n = temp%n;
+	}
+	
+	return m;
+}	
+
 int main(void) {
 	
-	m = 12;
-	n = 6;
+	unsigned int m, n;
 	
-	printf("Der ggt von %u und %u ist %u", m, n, ggt(m,n));
+	printf("Gib hier die erste Zahl ein: \n");
+	m = getchar();
+	
+	printf("Gib hier die zweite Zahl ein: \n");
+	n = getchar();
+	
+	printf("/nDer ggT von %u und %u ist %u\n", m, n, ggT1(m,n));
+	printf("Der verbesserte ggT von %u und %u ist %u\n", m, n, ggT2(m,n));
 	
 	return 0;	
 }
